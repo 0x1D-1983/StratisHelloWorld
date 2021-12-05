ffe5bf8dcad41927c9979ce5524a6e0795c356c9
Janos Kelemen <janos.kelemen@etxcapital.com>
2021-12-05 23:45:15 +0000

    Simple Hello World Stratis contract in C#

---
+++ HelloWorld.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Stratis.SmartContracts;
+
+
+    [Deploy]
+    public class HelloWorld : SmartContract
+    {
+        private string Greeting
+        {
+            get
+            {
+                return State.GetString("Greeting");
+            }
+            set
+            {
+                State.SetString("Greeting", value);
+            }
+        }
+
+        public HelloWorld(ISmartContractState contractState) : base(contractState)
+        {
+            Greeting = "Hello World!";
+        }
+
+        public string SayHello()
+        {
+            return Greeting;
+        }
+    }
+
+
